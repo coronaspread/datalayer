058eefde82adca552128a3cc9121a1d1fa3290b8
alexn11 <alexn11.gh@gmail.com>
2020-03-28 14:25:22 +0000

    uk harmonizer script

--- src/project/components/uk/ukglobals.py
+++ src/project/components/uk/ukglobals.py
@@ -7,3 +7,12 @@ local_data_path = "."
 
 final_data_path = "."
 final_data_file_name = "uk.csv"
+
+column_names = [ "date", "country_iso", "region_code", "region_code_native", "lat", "long", "hosp_with_symptons", "icu_case", "total_hospital", "home_insultation", "new_cummulate_positive", "discharge_and_healed", "deceased", "total_cases", "testing", "predictions", "population", "density", "over_65", "over_65_pop", "beds", "beds_per_capita", ]
+
+# TODO: fill this once the column names is fixed
+date_name_key = "date"
+#country_name_key = ""
+region_name_key = "region_code"
+cumulated_deaths_key = "deceased"
+cumulated_cases_key = "total_cases"
--- src/project/components/uk/ukharmonizer.py
+++ src/project/components/uk/ukharmonizer.py
@@ -3,16 +3,16 @@
 import os
 import pandas
 
-import ukglobals
+from ukglobals import *
 
 # note: this is the number of cumulated cases not current
 
 
-columns = [ "date", "country_iso", "region_code", "region_code_native", "lat", "long", "hosp_with_symptons", "icu_case", "total_hospital", "home_insultation", "new_cummulate_positive", "discharge_and_healed", "deceased", "total_cases", "testing", "predictions", "population", "density", "over_65", "over_65_pop", "beds", "beds_per_capita", ]
+columns = column_names
 
 data = pandas . DataFrame (columns = columns)
 
-regional_data = pandas . read_csv (os .path . join (ukglobals . local_data_path, ukglobals . regional_confirmed_cases_file_name))
+regional_data = pandas . read_csv (os .path . join (local_data_path, regional_confirmed_cases_file_name))
 
 
 nb_regional_data_items = len (regional_data)
@@ -20,16 +20,16 @@ nb_regional_data_items = len (regional_data)
 new_items = {}
 for column_name in columns :
   new_items [column_name] = nb_regional_data_items * [ "<NA>", ]
-new_items ["date"] = regional_data ["Date"] . values
+new_items [date_name_key] = regional_data ["Date"] . values
 new_items ["country_iso"] = nb_regional_data_items * [ "GBR", ]
-new_items ["region_code"] = regional_data ["Area"] . values
-new_items ["total_cases"] = regional_data ["TotalCases"] . values
+new_items [region_name_key] = regional_data ["Area"] . values
+new_items [cumulated_cases_key] = regional_data ["TotalCases"] . values
 
 data = data . append (pandas . DataFrame (new_items), sort = False)
 
 
 
-country_data = pandas . read_csv (os .path . join (ukglobals . local_data_path, ukglobals . country_indicators_file_name))
+country_data = pandas . read_csv (os .path . join (local_data_path, country_indicators_file_name))
 
 new_items = {}
 
@@ -38,15 +38,15 @@ for index, row in country_data . iterrows () :
   new_item = {}
   for column_name in columns :
     new_item [column_name] = "<NA>"
-  new_item ["date"] = row ["Date"]
+  new_item [date_name_key] = row ["Date"]
   new_item ["country_iso"] = "GBR"
-  new_item ["region_code"] = row ["Country"]
+  new_item [region_name_key] = row ["Country"]
 
   indicator = row ["Indicator"]
   if (indicator == "ConfirmedCases") :
-    indicator_key = "total_cases"
+    indicator_key = cumulated_cases_key
   elif (indicator == "Deaths") :
-    indicator_key = "deceased"
+    indicator_key = cumulated_deaths_key
   item_key = new_item ["date"] + new_item ["region_code"]
   is_new = False
   try :
@@ -59,7 +59,7 @@ for index, row in country_data . iterrows () :
 
 data = data . append (pandas . DataFrame (new_items . values ()), sort = False)
 
-data . to_csv (os . path . join (ukglobals . final_data_path, ukglobals . final_data_file_name))
+data . to_csv (os . path . join (final_data_path, final_data_file_name))
 
 
   #related_rows = country_data [country_data . Date == new_item ["date"]] [country_data . Country == new_item ["region_code"]]
