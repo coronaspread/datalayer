7cdd542546bb48c028bdc03fbd6f8ccace0d88c6
Bileam Scheuvens <benedictbileam@gmx.de>
2020-03-28 19:12:58 +0100

    add return statement on get_raw_data()

--- src/project/components/austria_management.py
+++ src/project/components/austria_management.py
@@ -7,7 +7,7 @@ class AustriaManager(CountryManager):
     aut = pd.read_csv(BASE_URL_AUSTRIA, sep=",")
 
     def download(self):
-        aut.to_csv("austria.csv", encoding="utf-8-sig", index=False)
+        self.aut.to_csv("austria.csv", encoding="utf-8-sig", index=False)
         return self
 
     def get_raw_data(self) -> pd.DataFrame:
@@ -15,28 +15,28 @@ class AustriaManager(CountryManager):
 
         :return: the raw data dataframe
         '''
-        pass
+        return self.aut
 
     def harmonized(self) -> pd.DataFrame:
         '''
 
         :return: the harmonized dataframe
         '''
-        aut2 = aut.drop(["OBJECTID", "infizierte_pro_ew", "zuwachs", "zuwachs_prozent", "einwohner"], axis=1)
+        self.aut2 = self.aut.drop(["OBJECTID", "infizierte_pro_ew", "zuwachs", "zuwachs_prozent", "einwohner"], axis=1)
         colnames = ["region_small_code_native", "region_small_name", "region_large_code_native", "region_large_name",
                    "no_cases", "Shape_Length", "Shape_Area", "report_date"]
-        aut2.columns = colnames
+        self.aut2.columns = colnames
 
-        aut2["uuid"] = ""
-        aut2["source"] = "Austria"
-        aut2["country_name"] = "Austria"
-        aut2["country_iso"] = "AUT"
+        self.aut2["uuid"] = ""
+        self.aut2["source"] = "Austria"
+        self.aut2["country_name"] = "Austria"
+        self.aut2["country_iso"] = "AUT"
 
-        aut2 = aut2[["uuid", "source", "report_date", "country_name", "country_iso", "region_large_name", "region_large_code_native",
+        self.aut2 = self.aut2[["uuid", "source", "report_date", "country_name", "country_iso", "region_large_name", "region_large_code_native",
 
                     "region_small_name", "region_small_code_native", "Shape_Length", "Shape_Area", "no_cases"]]
-        aut2.to_csv("austria_harmonized.csv", encoding="utf-8-sig", index=False)
-        return aut2
+        self.aut2.to_csv("austria_harmonized.csv", encoding="utf-8-sig", index=False)
+        return self.aut2
 
 
 if __name__ == '__main__':
