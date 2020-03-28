00f88058616e9ab8b29f5ef5b028390ed2668840
Marius Bild <mbild@phys.ethz.ch>
2020-03-28 11:17:33 +0100

    added missing return statement > china/download_china.py

--- src/project/components/china/download_china.py
+++ src/project/components/china/download_china.py
@@ -6,3 +6,5 @@ def donwload_china_raw(url= None):
     if url is None: 
         url='https://raw.githubusercontent.com/beoutbreakprepared/nCoV2019/master/latest_data/latestdata.csv'
     df = pd.read_csv(url, index_col= None, sep=',', error_bad_lines=True)
+
+    return df
