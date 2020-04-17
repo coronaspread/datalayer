500f265494d7ed99d5f1cfe7fba5def5aae8eb10
d435930 <richard-lei@gmx.de>
2020-04-17 18:43:24 +0200

    Add exception regarding not correctly harmonized datasets

--- src/general/exceptions.py
+++ src/general/exceptions.py
@@ -1,47 +1,29 @@
 class IllegalStateException(RuntimeError):
     """Exception raised when the state is illegal"""
 
-    def __repr__(self, message):
-        return message
-
 
 class IllegalArgumentException(ValueError):
     """Exception raised when an argument is illegal"""
-
-    def __repr__(self, message):
-        return message
 
 
 class OptimisationWarning(RuntimeWarning):
     """Warning raised when the Optimisation does not converge"""
 
-    def __repr__(self, message):
-        return message
-
 
 class OptimisationError(RuntimeError):
     """Error raised when the Optimisation does not converge"""
-
-    def __repr__(self, message):
-        return message
 
 
 class NumericInstabilityWarning(RuntimeWarning):
     """Warning raised when the numeric instability is to be expected"""
 
-    def __repr__(self, message):
-        return message
-
 
 class ArgumentWarning(RuntimeWarning):
     """Warning raised when an Argument is not as expected"""
-
-    def __repr__(self, message):
-        return message
 
 
 class IllegalFunctionCallWarning(RuntimeWarning):
     """Warning raised when an Argument is not as expected"""
 
-    def __repr__(self, message):
-        return message
+class InvalidDataModel(RuntimeWarning):
+    """Warning raised when dataset does not have a valid model"""
